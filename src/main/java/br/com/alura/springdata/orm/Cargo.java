34b20218fa9d4850d910b94551848418bd24b025
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-08-19 16:20:55 -0300

    creating repository

--- src/main/java/br/com/alura/springdata/orm/Cargo.java
+++ src/main/java/br/com/alura/springdata/orm/Cargo.java
@@ -1,4 +1,29 @@
 package br.com.alura.springdata.orm;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cargos")
 public class Cargo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
