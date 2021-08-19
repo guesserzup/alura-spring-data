2929e9e61e6842932d71029c606f67c9441ac72d
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-08-19 16:26:20 -0300

    creating a scanner

--- src/main/java/br/com/alura/springdata/SpringDataApplication.java
+++ src/main/java/br/com/alura/springdata/SpringDataApplication.java
@@ -1,13 +1,34 @@
 package br.com.alura.springdata;
 
+import br.com.alura.springdata.orm.Cargo;
+import br.com.alura.springdata.repository.CargoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner {
+
+	@Autowired
+	CargoRepository cargoRepository;
+
+	public SpringDataApplication(CargoRepository cargoRepository) {
+		this.cargoRepository = cargoRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("Desenvolvedor de software");
+
+		cargoRepository.save(cargo);
+	}
 }
