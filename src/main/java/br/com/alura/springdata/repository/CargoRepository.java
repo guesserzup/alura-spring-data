2bf4edaf48b79b9eba5851a2df171da60cb9de61
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-08-20 10:57:45 -0300

    fix

--- src/main/java/br/com/alura/springdata/repository/CargoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.alura.springdata.repository;
-
-import br.com.alura.springdata.orm.Cargo;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CargoRepository extends CrudRepository<Cargo, Long> {
-}
